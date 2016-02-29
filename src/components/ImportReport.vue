56853f1a8bd8e39846109abbc1088f28785d0027
Zach Briggs <briggszj@gmail.com>
2016-02-29 00:42:08 -0600

    Add check for missing variables

--- src/components/ImportReport.vue
+++ src/components/ImportReport.vue
@@ -4,60 +4,102 @@
 
     <h2>Summary</h2>
     <ul>
-      <li>{{ sheetA.length }} rows imported from Sheet A</li>
-      <li>{{ sheetB.length }} rows imported from Sheet B</li>
+      <li>{{ sheetAIds.length }} rows imported from Sheet A</li>
+      <li>{{ sheetBIds.length }} rows imported from Sheet B</li>
     </ul>
 
+    <h2>Details</h2>
+
     <div v-if="sheetADupes.length === 0">
-      <h2>No duplicate Patient IDs found in Sheet A</h2>
+      <div>No duplicate Patient IDs found in Sheet A</div>
     </div>
     <div v-if="sheetADupes.length > 0">
-      <h2>Sheet A Dupes</h2>
+      <div>Sheet A Dupes</div>
       <ul>
         <li v-for="dupe in sheetADupes" track-by="$index">{{ dupe }}</li>
       </ul>
     </div>
+
     <div v-if="sheetBDupes.length === 0">
-      <h2>No duplicate Patient IDs found in Sheet B</h2>
+      <div>No duplicate Patient IDs found in Sheet B</div>
     </div>
     <div v-if="sheetBDupes.length > 0">
-      <h2>Sheet B Dupes</h2>
+      <div>Sheet B Dupes</div>
       <ul>
         <li v-for="dupe in sheetBDupes" track-by="$index">{{ dupe }}</li>
       </ul>
     </div>
+
     <div v-if="missingSheetAIds.length === 0">
-      <h2>Sheet A has all of Sheet B's IDs</h2>
+      <div>Sheet A has all of Sheet B's IDs</div>
     </div>
     <div v-if="missingSheetAIds.length > 0">
-      <h2>{{ missingSheetAIds.length }} IDs missing from Auditor A</h2>
+      <div>{{ missingSheetAIds.length }} IDs missing from Auditor A</div>
       <ul>
         <li v-for="missingId in missingSheetAIds" track-by="$index">{{ missingId }}</li>
       </ul>
     </div>
+
     <div v-if="missingSheetBIds.length === 0">
-      <h2>Sheet B has all of Sheet A's IDs</h2>
+      <div>Sheet B has all of Sheet A's IDs</div>
     </div>
     <div v-if="missingSheetBIds.length > 0">
-      <h2>{{ missingSheetBIds.length }} IDs found in Sheet A are missing from Sheet B</h2>
-      <ul>
+      <div>{{ missingSheetBIds.length }} IDs found in Sheet A are missing from Sheet B</div>
+      <button v-on:click="displayMissingBDetails = !displayMissingBDetails">
+        Show {{ displayMissingBDetails ? 'Less' : 'More' }}
+      </button>
+      <ul v-if="displayMissingBDetails">
         <li v-for="missingId in missingSheetBIds" track-by="$index">{{ missingId }}</li>
       </ul>
     </div>
+
+    <div v-if="emptySheetAVariables.length === 0">
+      <div>Sheet A has no empty variables</div>
+    </div>
+    <div v-if="emptySheetAVariables.length > 0">
+      <div>
+        Sheet A has {{ emptySheetAVariables.length }} rows with a patient ID and at least 1 missing variable
+      </div>
+      <ul>
+        <li v-for="data in emptySheetAVariables">
+          {{ data.id }}
+          <span v-for="emptyVariable in data.emptyVariables">
+            {{ emptyVariable }}
+          </span>
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="emptySheetBVariables.length === 0">
+      <div>Sheet A has no empty variables</div>
+    </div>
+    <div v-if="emptySheetBVariables.length > 0">
+      <div>
+        Sheet A has {{ emptySheetBVariables.length }} rows with a patient ID and at least 1 missing variable
+      </div>
+      <ul>
+        <li v-for="data in emptySheetBVariables">
+          {{ data.id }}
+          <span v-for="emptyVariable in data.emptyVariables">
+            {{ emptyVariable }}
+          </span>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
 <script>
 import store from '../store'
 import sheetKeys from '../sheet-keys'
-const { PT_ID_KEY } = sheetKeys
+const { PT_ID_KEY, QUESTION_KEYS } = sheetKeys
+window.store = store
 
 export default {
   data() {
     return {
-      sheetA: store.state.sheetA,
-
-      sheetB: store.state.sheetB
+      displayMissingBDetails: false,
     }
   },
 
@@ -77,7 +119,12 @@ export default {
       })
 
       return patientIds
-    }
+    },
+
+    emptyVariables(row) {
+      return QUESTION_KEYS.filter(key => !row[key])
+    },
+
   },
 
   computed: {
@@ -102,6 +149,30 @@ export default {
 
     missingSheetBIds() {
       return this.sheetAIds.filter(id => !this.sheetBIds.find(bId => id === bId))
+    },
+
+    emptySheetAVariables() {
+      return store.state.sheetA
+      .map(row => {
+        const emptyVariables = this.emptyVariables(row)
+        return {
+          id: row[PT_ID_KEY],
+          emptyVariables: emptyVariables
+        }
+      })
+      .filter(data => data.emptyVariables.length > 0)
+    },
+
+    emptySheetBVariables() {
+      return store.state.sheetB
+      .map(row => {
+        const emptyVariables = this.emptyVariables(row)
+        return {
+          id: row[PT_ID_KEY],
+          emptyVariables: emptyVariables
+        }
+      })
+      .filter(data => data.emptyVariables.length > 0)
     },
   }
 
--- src/store.js
+++ src/store.js
@@ -4,7 +4,21 @@ import sheetKeys from './sheet-keys'
 
 Vue.use(Vuex)
 
-const { PT_ID_KEY } = sheetKeys
+const { PT_ID_KEY, QUESTION_KEYS } = sheetKeys
+
+
+function cleanKeys(row) {
+  const keyMaps = Object.keys(row).map(key => {
+    return {
+      originalKey: key,
+      cleanedKey: key.trim()
+    }
+  })
+
+  const cleanedRow = {}
+  keyMaps.forEach(keyMap => cleanedRow[keyMap.cleanedKey] = row[keyMap.originalKey])
+  return cleanedRow
+}
 
 const store = new Vuex.Store({
   strict: true,
@@ -17,11 +31,11 @@ const store = new Vuex.Store({
 
   mutations: {
     LOAD_SHEET_A (state, sheetA) {
-      state.sheetA = sheetA.filter(row => row[PT_ID_KEY])
+      state.sheetA = sheetA.filter(row => row[PT_ID_KEY]).map(cleanKeys)
     },
 
     LOAD_SHEET_B (state, sheetB) {
-      state.sheetB = sheetB.filter(row => row[PT_ID_KEY])
+      state.sheetB = sheetB.filter(row => row[PT_ID_KEY]).map(cleanKeys)
     }
   }
 })
