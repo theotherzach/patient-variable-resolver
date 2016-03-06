9261b7c0e6164273b6700f8ef5dfe5929035d9c3
Zach Briggs <briggszj@gmail.com>
2016-03-06 16:12:12 -0600

    Extract VariableCard

--- src/components/Audit.vue
+++ src/components/Audit.vue
@@ -14,28 +14,9 @@
   </p>
 
   <div class="container">
-    <div v-for="variable in problemVariables"
-      class="variableCard">
-      <div class="title">{{ variable.name }}</div>
-      <div class="content">
-        <table>
-          <tr>
-            <th>A1</th>
-            <th>{{ audit.auditorB }}</th>
-          </tr>
-          <tr>
-            <td>{{ variable.aAnswer }}</td>
-            <td>{{ variable.bAnswer }}</td>
-          </tr>
-          <tr>
-            <td colspan="2"><input v-model="resolution"></td>
-          </tr>
-          <tr>
-            <td colspan="2"><button>Resolve</button></td>
-          </tr>
-        </table>
-      </div>
-    </div>
+    <variable-card v-for="variable in problemVariables"
+      :variable="variable" :audit="audit">
+    </variable-card>
   </div>
 
 </div>
@@ -44,48 +25,32 @@
 <script>
 import sheetKeys from '../sheet-keys'
 const { PT_ID_KEY } = sheetKeys
+import VariableCard from './VariableCard.vue'
 
 export default {
-  data() {
-  },
-
-  methods: {
-
-    resolve(variable, answer) {
-    }
-
+  components: {
+    VariableCard
   },
 
   computed: {
-
-    resolutions: {
-      get() {
-        return this.audit.variables.reduce((memo, variable) => {
-          memo[variable.patientId] = {
-            name: variable.name,
-            patientId: variable.patientId,
-            answer: ''
-          }
-          return memo
-        }, {})
-      }
-
-    },
 
     problemVariables() {
       return this.audit.variables.filter(variable => {
         return variable.aAnswer !== variable.bAnswer 
       })
     }
+
   },
 
   vuex: {
     state: {
+
       audit(state) {
         return state.audits.filter(audit => {
             return audit.patientId === this.$route.params.patientId
         })[0]
       }
+
     }
   }
 }
@@ -97,56 +62,4 @@ export default {
   margin-right: auto;
 }
 
-.cell {
-  width: 50%;
-}
-
-.variableCard {
-  display: inline-block;
-  vertical-align: top;
-  width: 200px;
-  margin-right: 20px;
-  margin-bottom: 20px;
-  border: 1px solid #ddd;
-
-  .title {
-    padding: 8px;
-    background: #42b983;
-    font-weight: 600;
-    color: #fff;
-  }
-
-  .content {
-  }
-
-  input, button {
-    width: 66px;
-    height: 50px;
-  }
-
-  table, caption, tbody, tfoot, thead, tr, th, td {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-
-  th {
-    font-weight: 600;
-  }
-
-  th, td {
-    width: 100px;
-    height: 66px;
-    text-align: center;
-    vertical-align: middle;
-    border: 1px solid #ddd;
-  }
-}
 </style>
---
+++ src/components/VariableCard.vue
@@ -0,0 +1,81 @@
+<template>
+<div class="variableCard">
+  <div class="title">{{ variable.name }}</div>
+  <div class="content">
+    <table>
+      <tr>
+        <th>A1</th>
+        <th>{{ audit.auditorB }}</th>
+      </tr>
+      <tr>
+        <td>{{ variable.aAnswer }}</td>
+        <td>{{ variable.bAnswer }}</td>
+      </tr>
+      <tr>
+        <td colspan="2"><input v-model="resolution"></td>
+      </tr>
+      <tr>
+        <td colspan="2"><button>Resolve</button></td>
+      </tr>
+    </table>
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    props: ['variable', 'audit']
+  }
+</script>
+
+<style lang="scss">
+.variableCard {
+  display: inline-block;
+  vertical-align: top;
+  width: 200px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  border: 1px solid #ddd;
+
+  .title {
+    padding: 8px;
+    background: #42b983;
+    font-weight: 600;
+    color: #fff;
+  }
+
+  .content {
+  }
+
+  input, button {
+    width: 66px;
+    height: 50px;
+  }
+
+  table, caption, tbody, tfoot, thead, tr, th, td {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  th {
+    font-weight: 600;
+  }
+
+  th, td {
+    width: 100px;
+    height: 66px;
+    text-align: center;
+    vertical-align: middle;
+    border: 1px solid #ddd;
+  }
+}
+</style>
+
